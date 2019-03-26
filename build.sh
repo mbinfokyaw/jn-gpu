f53ac6a93ec2e5c581685112536b4cb5e38dc96a
brundquist <brandon.rundquist@davita.com>
2019-03-26 06:27:04 -0600

    -> name

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-
 build_and_push () {
 
   export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-  docker build -t $DOCKER_USER/$1:$COMMIT -f $1/Dockerfile $1
-  docker tag $DOCKER_USER/$1:$COMMIT $DOCKER_USER/$1:$TAG
-  docker tag $DOCKER_USER/$1:$COMMIT $DOCKER_USER/$1:travis-$TRAVIS_BUILD_NUMBER
-  docker push $DOCKER_USER/$1
+  docker build -t $DOCKER_USERNAME/$1:$COMMIT -f $1/Dockerfile $1
+  docker tag $DOCKER_USERNAME/$1:$COMMIT $DOCKER_USERNAME/$1:$TAG
+  docker tag $DOCKER_USERNAME/$1:$COMMIT $DOCKER_USERNAME/$1:travis-$TRAVIS_BUILD_NUMBER
+  docker push $DOCKER_USERNAME/$1
 
 }
 
