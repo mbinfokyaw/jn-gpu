2dd27723eaac79ddf0cba90a2f150e6c88d397b9
Brandon Rundquist <Brandon.Rundquist@davita.com>
2019-08-10 06:00:11 -0600

    echo tag

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ build_and_push () {
   export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
   docker build -t $DOCKER_USERNAME/$1:$COMMIT -f $1/Dockerfile $1
   
-  if [ -z ${TRAVIS_TAG} ]; then 
+  if [ -z ${TRAVIS_TAG} ]; then
+    echo "Tag Docker image with Git tag $TRAVIS_TAG"
     docker tag $DOCKER_USERNAME/$1:$COMMIT $DOCKER_USERNAME/$1:$TRAVIS_TAG; 
   else 
     echo "No tag supplied."; 
