f98454e6504f8502b3871f08dd6a3584083fc2cd
Brandon Rundquist <Brandon.Rundquist@davita.com>
2019-08-10 05:44:55 -0600

    add tag to build

--- build.sh
+++ build.sh
@@ -8,6 +8,13 @@ build_and_push () {
 
   export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
   docker build -t $DOCKER_USERNAME/$1:$COMMIT -f $1/Dockerfile $1
+  
+  if [ -z ${TRAVIS_TAG} ]; then 
+    docker tag $DOCKER_USERNAME/$1:$COMMIT $DOCKER_USERNAME/$1:$TRAVIS_TAG; 
+  else 
+    echo "No tag supplied."; 
+  fi
+  
   docker tag $DOCKER_USERNAME/$1:$COMMIT $DOCKER_USERNAME/$1:$TAG
   docker tag $DOCKER_USERNAME/$1:$COMMIT $DOCKER_USERNAME/$1:travis-$TRAVIS_BUILD_NUMBER
   docker push $DOCKER_USERNAME/$1
